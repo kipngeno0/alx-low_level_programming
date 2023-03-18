fcd5c05d268637a974902e99ce17708f6d523c39
kipngeno0 <kipngenomoses2000@gmail.com>
2023-03-17 17:19:28 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,32 +3,22 @@
 #include <stdio.h>
 
 /**
- * main - main points
- *
- * Return: always 0
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
-	int m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10;
-	if (m > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5", n, m);
-	}
-	else if (m == 0)
-	{
-		printf("Last digit of %d is %d and is 0", n, m);
-	}
-	else
-	{
-		printf("Last digit of %d  is %d  and is less than 6 and not 0", n, m);
-	}
-	printf("\n");
-
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,26 +6,19 @@
  */
 int main(void)
 {
-	int i;
-	int j;
-
-	for (i = 0; i < 10 ; i++)
-	{
-		for (j = 1 ; j < 10 ; j++)
-		{
-			if (i < j && i != j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i + j != 17)
-				{
-					putchar('.');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+    for (int i = 0; i <= 8; i++) {
+        for (int j = i + 1; j <= 9; j++) {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(',');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i != 8 || j != 9) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    return 0;
 }
-
